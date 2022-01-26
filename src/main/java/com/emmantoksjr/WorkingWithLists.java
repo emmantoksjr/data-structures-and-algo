47043f48590458896e776ca6de2bdf1a44ac7672
emmanuel <emmantoksjr@gmail.com>
2022-01-26 14:30:08 +0100

    Working with Lists

---
+++ src/main/java/com/emmantoksjr/WorkingWithLists.java
@@ -0,0 +1,21 @@
+package com.emmantoksjr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkingWithLists {
+    public static void main(String[] args) {
+        List<String> colors = new ArrayList<>();
+        colors.add("this");
+        colors.add("that");
+
+        System.out.println(colors.contains("this"));
+        System.out.println(colors.size());
+
+        for (String color: colors) {
+            System.out.println(color);
+        }
+
+        colors.forEach(System.out::println);
+    }
+}
